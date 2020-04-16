f3f6d2f1b4f7746d6e4bfefe752180d50ab451cf
JJ Garlitos <jj.garlitos@madelabs.io>
2020-04-15 22:00:11 -0600

    Added Comment component

---
+++ clientsrc/src/components/Comment.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="comment">
+    <p>This is a comment</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Comment",
+  props: {
+    // comment: { type: Object, required: true }
+  }
+};
+</script>
+
+<style>
+</style>
